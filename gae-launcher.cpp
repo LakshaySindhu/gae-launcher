4247ec051d993bd3cbd1b653e7bf0279df01ab4d
LakshaySindhu <137389809+LakshaySindhu@users.noreply.github.com>
2024-02-18 20:26:30 +0530

    Create gae-launcher.cpp

---
+++ gae-launcher.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  cout<<"Using Google App Engine to Launch our App - Cloud Computing Experiment "<<endl;
+  cout<<"Name : "<<"Lakshay Sindhu"<<endl;
+  cout<<"UID : "<<"21BCS10420"<<endl;
+
+  return 0;
+}
